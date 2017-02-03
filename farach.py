ea4a61ecda7c24c3e9b86521df631062c50f95cb
skodborg <skod92@gmail.com>
2017-02-03 16:31:09 +0100

    fixed first bug in radix sort; now correctly sorts tuples and does not just combine the tuples to numbers, which creates complications for tuples containing multiple-digit numbers

--- farach.py
+++ farach.py
@@ -24,12 +24,12 @@ def T_odd(inputstr):
 
     # strings are 0-indexed in python, and 1-indexed in the book's examples
     # so we -2 and -1 from pos 'i' to let our match the book's examples
-    chr_pairs = [int(S[2*i-2] + S[2*i-1]) for i in range(1, math.floor(n / 2) + 1)]
-    assert chr_pairs == [12, 11, 12, 21, 22, 21]
+    chr_pairs = [(int(S[2*i-2]), int(S[2*i-1])) for i in range(1, math.floor(n / 2) + 1)]
+    assert chr_pairs == [(1, 2), (1, 1), (1, 2), (2, 1), (2, 2), (2, 1)]
 
     # sort in O(k * n) using radix sort (k = 2 here, guaranteed)
     radixsort.sort(chr_pairs)
-    assert chr_pairs == [11, 12, 12, 21, 21, 22]
+    assert chr_pairs == [(1, 1), (1, 2), (1, 2), (2, 1), (2, 1), (2, 2)]
     
     # remove duplicates, O(n)
     unique_chr_pairs = [chr_pairs[0]]
@@ -37,7 +37,7 @@ def T_odd(inputstr):
         if unique_chr_pairs[-1] != pair:
             unique_chr_pairs.append(pair)
     chr_pairs = unique_chr_pairs
-    assert chr_pairs == [11, 12, 21, 22]
+    assert chr_pairs == [(1, 1), (1, 2), (2, 1), (2, 2)]
     
     # compute S'[i] = rank of (S[2i - 1], S[2i])
     Sm = ''
@@ -47,10 +47,11 @@ def T_odd(inputstr):
         pair2single[pair] = count
         count += 1
     for i in range(1, math.floor(n / 2) + 1):
-        pair = int(S[2*i-2] + S[2*i-1])
+        pair = (int(S[2*i-2]), int(S[2*i-1]))
         Sm += str(pair2single[pair])
     Sm += unique_char
     assert Sm == '212343$'
+
 
     print('Not implemented yet')
 
