24c7e9e008931526c61d0a75c0c31ad0b201794d
skodborg <skod92@gmail.com>
2017-02-08 11:59:13 +0100

    modified radixsort to enable stable sorting by only one index in the tuples in the given tuple list)

--- radixsort.py
+++ radixsort.py
@@ -1,60 +1,68 @@
 # source: http://www.geekviewpoint.com/python/sorting/radixsort
+def sort(aTupleList, tup_idx=None):
+    def sort_helper(aTupleList, tup_idx):
+        RADIX = 10
+        maxLength = False
+        tmp, placement = -1, 1
 
-def sort(aTupleList):
-  def sort_helper(aTupleList, tup_idx):
-    RADIX = 10
-    maxLength = False
-    tmp, placement = -1, 1
-   
-    while not maxLength:
-        maxLength = True
-        # declare and initialize buckets
-        buckets = [list() for _ in range(RADIX)]
+        while not maxLength:
+            maxLength = True
+            # declare and initialize buckets
+            buckets = [list() for _ in range(RADIX)]
 
-        # split aTupleList between lists
-        for tup in aTupleList:
-            # if tup[1] == '$':
-                
-            tmp = int(tup[tup_idx] / placement)
-            buckets[tmp % RADIX].append(tup)
-            if maxLength and tmp > 0:
-                maxLength = False
-        # print(buckets)
-        # empty lists into aTupleList array
-        a = 0
-        for b in range(RADIX):
-            buck = buckets[b]
-            for i in buck:
-                aTupleList[a] = i
-                a += 1
-        # move to next digit
-        placement *= RADIX
-  sort_helper(aTupleList, 1)
-  sort_helper(aTupleList, 0)
+            # split aTupleList between lists
+            for tup in aTupleList:
+                # if tup[1] == '$':
+
+                tmp = int(tup[tup_idx] / placement)
+                buckets[tmp % RADIX].append(tup)
+                if maxLength and tmp > 0:
+                    maxLength = False
+            # print(buckets)
+            # empty lists into aTupleList array
+            a = 0
+            for b in range(RADIX):
+                buck = buckets[b]
+                for i in buck:
+                    aTupleList[a] = i
+                    a += 1
+            # move to next digit
+            placement *= RADIX
+
+    if tup_idx != None:
+        sort_helper(aTupleList, tup_idx)
+    else:
+        sort_helper(aTupleList, 1)
+        sort_helper(aTupleList, 0)
 
 
 def test_sort():
-    l = [(2, 11), (1, 11)]
-    sort(l)
-    assert l == [(1, 11), (2, 11)]
+    testlist = [(2, 11), (1, 11)]
+    sort(testlist)
+    assert testlist == [(1, 11), (2, 11)]
 
-    l = [(2, 22), (2, 11)]
-    sort(l)
-    assert l == [(2, 11), (2, 22)]
+    testlist = [(2, 22), (2, 11)]
+    sort(testlist)
+    assert testlist == [(2, 11), (2, 22)]
 
-    l = [(1, 22), (2, 11)]
-    sort(l)
-    assert l == [(1, 22), (2, 11)]
+    testlist = [(1, 22), (2, 11)]
+    sort(testlist)
+    assert testlist == [(1, 22), (2, 11)]
 
-    l = [(2, 22), (1, 33), (2, 11)]
-    sort(l)
-    assert l == [(1, 33), (2, 11), (2, 22)]
+    testlist = [(2, 22), (1, 33), (2, 11)]
+    sort(testlist)
+    assert testlist == [(1, 33), (2, 11), (2, 22)]
 
-    l = [(2, 22), (1, '$'), (1, 33), (2, 11)]
-    sort(l)
-    assert l == [(1, 33), (1, '$'), (2, 11), (2, 22)]
+    testlist = [(2, 22), (1, 33), (2, 11)]
+    sort(testlist, 1)
+    assert testlist == [(2, 11), (2, 22), (1, 33)]
+
+    testlist = [(2, 22), (1, 33), (2, 11)]
+    sort(testlist, 0)
+    assert testlist == [(1, 33), (2, 22), (2, 11)]
 
     print('Unit tests succeeded!')
+
 
 def main():
     test_sort()
