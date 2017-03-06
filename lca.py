505a46d8b5d8c2c06d6f7bb13d047a845147cf95
skodborg <skod92@gmail.com>
2017-03-06 12:29:48 +0100

    added lca.py file

---
+++ lca.py
@@ -0,0 +1,17 @@
+from utils import Node
+
+
+def preprocess(tree):
+    print('Not implemented yet')
+
+
+def query(node1, node2):
+    print('Not implemented yet')
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
