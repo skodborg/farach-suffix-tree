cc104d604d1e902feab15d4bdde3ba15b64bee30
Simon Fischer <mail@simonfischer.com>
2017-05-02 12:28:39 +0200

    added dict to mccreight

--- mccreight.py
+++ mccreight.py
@@ -39,18 +39,32 @@ def slowscan(u, v, S):
        
             # make new internal node
             internal = Node(aId='inner')
-            # TODO: ADD LIST HERE
+            internal.charDict = dict()
             # internal.str = curr_node.str + curr_lcp
             # internal.edge = curr_lcp
             internal.leaflist = child.leaflist
             internal.str_length = curr_node.str_length + string_length(curr_lcp) 
 
-    
+
+            # Remove child
+            childLeafId = child.leaflist[0].id-1
+            childChar = S[childLeafId + child.parent.str_length]
+            del curr_node.charDict[childChar]
             curr_node.remove_child(child)
+
+            # Add internal node
             curr_node.add_child(internal)
-            # TODO: UPDATE LIST HERE
+            internalLeafId = internal.leaflist[0].id-1
+            internalChar = S[internalLeafId + internal.parent.str_length]
+            curr_node.charDict[internalChar] = internal
+            
+
+            curr_node.charDict
+            # Add child
             internal.add_child(child)
-            # TODO: UPDATE LIST HERE
+            childChar = S[childLeafId + child.parent.str_length]
+            internal.charDict[childChar] = child
+    
 
             remaining = (remaining[0]+string_length(curr_lcp), remaining[1])
 
@@ -73,29 +87,36 @@ def fastscan(u, v, S):
             break
         found_child = False
 
-        for child in ui.children:
-            
-            # TODO: UPDATE THIS LOOP TO UTILIZE BINARY SEARCH INSTEAD
-            # in ui.children_char_list, look for entry v[idx]
-            #   if -1 is returned, then no child was found
-            #   if None is returned? no child was found
-            #   if a child was found, do whatever is done already as below
-            # THINK: is it necessary with binary search when list lookup at an
-            #        an index is an O(1)-operation? because of the memory layout
-            #        if so - why is McCreight then not plain O(n) running time?
 
-            # compare the first character on edges to all children of u
-            # if some edge matches current char in v, walk down this edge and
-            # reloop to look further down from this new node ui
-            leafID = child.leaflist[0].id - 1
-            first_char_edge = S[leafID + child.parent.str_length]
+        if S[v[0]+idx] in ui.charDict:
+            child = ui.charDict[S[v[0]+idx]]
+            prev_idx = idx
+            idx += (child.str_length - child.parent.str_length)
+            ui = child
+            found_child = True
+        # for child in ui.children:
+            
+        #     # TODO: UPDATE THIS LOOP TO UTILIZE BINARY SEARCH INSTEAD
+        #     # in ui.children_char_list, look for entry v[idx]
+        #     #   if -1 is returned, then no child was found
+        #     #   if None is returned? no child was found
+        #     #   if a child was found, do whatever is done already as below
+        #     # THINK: is it necessary with binary search when list lookup at an
+        #     #        an index is an O(1)-operation? because of the memory layout
+        #     #        if so - why is McCreight then not plain O(n) running time?
+
+        #     # compare the first character on edges to all children of u
+        #     # if some edge matches current char in v, walk down this edge and
+        #     # reloop to look further down from this new node ui
+        #     leafID = child.leaflist[0].id - 1
+        #     first_char_edge = S[leafID + child.parent.str_length]
            
-            if first_char_edge == S[v[0]+idx]:
-                prev_idx = idx
-                idx += (child.str_length - child.parent.str_length)
-                ui = child
-                found_child = True
-                break
+        #     if first_char_edge == S[v[0]+idx]:
+        #         prev_idx = idx
+        #         idx += (child.str_length - child.parent.str_length)
+        #         ui = child
+        #         found_child = True
+        #         break
         if not found_child:
             # looped through all children of current node ui, and none of them
             # shared paths with v; break out with ui being the node with the
@@ -140,25 +161,27 @@ def construct_suffix_tree(inputstr, printstuff=False):
             alphabet[c] = count
             count += 1
 
-    print(alphabet)
 
     n = len(S)
 
     id2node = {}
 
     root = Node(aId='root')
+    root.charDict = dict()
     root.str = root.edge = []
     root.suffix_link = root
-    root.children_char_list = [None] * (len(alphabet) + 1)
+
     fst_child = Node(aId=1, aStrLength=n)
+    fst_child.charDict = dict()
 
     # fst_child.str = fst_child.edge = S
-    fst_child.children_char_list = [None] * (len(alphabet) + 1)
 
     fst_child.str_length = n
     fst_child.leaflist = [fst_child]
-   
+
+    root.charDict[S[0]] = fst_child
     root.add_child(fst_child)
+
     # root.children_char_list[alphabet[fst_child.edge[0]]] = fst_child
     # print(root.children_char_list)
     root.leaflist = [fst_child]
@@ -176,6 +199,7 @@ def construct_suffix_tree(inputstr, printstuff=False):
 
             # add i+1 and head(i+1) as node if necessary
             leaf_iplus1 = Node(aId=i + 1)
+            leaf_iplus1.charDict = dict()
             # TODO: ADD LIST HERE
             # leaf_iplus1.str = S[i:]
             # leaf_iplus1.edge = remaining
@@ -185,11 +209,17 @@ def construct_suffix_tree(inputstr, printstuff=False):
             # tail_i = tail_i[len(head_i.str):]
             leaf_iplus1.str_length = n - i #TODO: correct index?
            
-            head_i.add_child(leaf_iplus1)
 
-            tail_i = (tail_i[0] + head_i.str_length, tail_i[1]) #tail_i[head_i.str_length:]
             leaf_iplus1.leaflist = [leaf_iplus1]
 
+            head_i.add_child(leaf_iplus1)
+
+            leaf_iplusLeafID = i
+            leaf_iplusChar = S[i + leaf_iplus1.parent.str_length]
+            head_i.charDict[leaf_iplusChar] = leaf_iplus1
+
+            tail_i = (tail_i[0] + head_i.str_length, tail_i[1]) #tail_i[head_i.str_length:]
+            
             continue
 
         u = head_i.parent
@@ -212,6 +242,7 @@ def construct_suffix_tree(inputstr, printstuff=False):
             parent = w.parent
             leaf = w
             w = Node(aId='inner')
+            w.charDict = dict()
 
             # w.edge = leaf.edge[: len(leaf.edge) - len(remaining)]
             # w.str = parent.str + w.edge
@@ -219,25 +250,41 @@ def construct_suffix_tree(inputstr, printstuff=False):
 
             w.str_length = leaf.str_length - string_length(remaining)
           
-            parent.add_child(w)
+
+            leafID  = leaf.leaflist[0].id - 1
+            leafChar = S[leaf.parent.str_length + leafID]
+            del parent.charDict[leafChar]
             parent.remove_child(leaf)
+
+
+            w.leaflist = leaf.leaflist
+
+            parent.add_child(w)
+            wID = w.leaflist[0].id - 1
+            wChar = S[w.parent.str_length + wID]
+            parent.charDict[wChar] = w
             # TODO: UPDATE LIST HERE
             w.add_child(leaf)
+            leafID = leaf.leaflist[0].id - 1
+            leafChar = S[leaf.parent.str_length + leafID]
+            w.charDict[leafChar] = leaf
             # TODO: UPDATE LIST HERE
-            w.leaflist = leaf.leaflist
 
         
             head_i.suffix_link = w
             head_i = w
 
             new_leaf = Node(aId=i + 1)
-
+            new_leaf.charDict = dict()
             # TODO: ADD LIST HERE
             # new_leaf.str = S[i:]
             # new_leaf.edge = new_leaf.str[len(w.str):]
             new_leaf.leaflist = [new_leaf]
             new_leaf.str_length = n - i #TODO: correct index?
             w.add_child(new_leaf)
+            new_leafID = new_leaf.leaflist[0].id - 1
+            new_leafChar = S[new_leaf.parent.str_length + new_leafID]
+            w.charDict[new_leafChar] = new_leaf
             # TODO: UPDATE LIST HERE
 
         else:
@@ -245,6 +292,8 @@ def construct_suffix_tree(inputstr, printstuff=False):
             head_i.suffix_link = w
             head_i, remaining = slowscan(w, tail_i, S)
             leaf_iplus1 = Node(aId=i + 1)
+            leaf_iplus1.charDict = dict()
+
             # TODO: ADD LIST HERE
             # leaf_iplus1.str = S[i:]
             # leaf_iplus1.edge = remaining
@@ -256,6 +305,11 @@ def construct_suffix_tree(inputstr, printstuff=False):
             leaf_iplus1.leaflist = [leaf_iplus1]
 
             head_i.add_child(leaf_iplus1)
+
+            leaf_iplus1ID = leaf_iplus1.leaflist[0].id - 1
+            leaf_iplus1Char = S[leaf_iplus1.parent.str_length + leaf_iplus1ID]
+            head_i.charDict[leaf_iplus1Char] = leaf_iplus1
+
             tail_i = (i + head_i.str_length, n)
 
     root.update_leaf_list()
--- speed_tests.py
+++ speed_tests.py
@@ -4,7 +4,7 @@ import random
 import string
 import traceback
 import time
-
+import mccreight
 
 def runFarach(inputstr):
 	S = farach.str2int(inputstr)
@@ -12,21 +12,15 @@ def runFarach(inputstr):
 
 
 def main():
-	total = []
-	i = 999
-	# for i in range(1, 100000, 1000):
-	while True:
+
+	for i in range(1, 100000, 1000):
 		S = ''.join(random.choice(string.digits) for _ in range(i))
 		start = time.time()
-		correct_tree = runFarach(S)
+		tree = mccreight.construct_suffix_tree(farach.str2int(S))
+		#correct_tree = runFarach(S)
 		end = time.time()
-		total.append((i, (end-start)))
 		i += 1000
-		# print("Total:")
-		# for n, t in total:
-		# 	if n == 1:
-		# 		continue
-		# 	print("%i, %f" % (n, t))
+		print(i, ",", (end - start))
 
 if __name__ == '__main__':
 	main()
