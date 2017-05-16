382f53cd53f9f3ef7088bec51d32472635511fe3
Simon Fischer <mail@simonfischer.com>
2017-05-16 11:09:44 +0200

    added changes to farach can now track recursive depth

--- memory_tracker.py
+++ memory_tracker.py
@@ -6,13 +6,13 @@ _proc = psutil.Process(os.getpid())
 _baseline = _proc.memory_info().rss >> 10
 _peak = 0
 
-
+_recursiveCalls = 0
 
 def rebase():
-	global _peak, _baseline
+	global _peak, _baseline, _recursiveCalls
 	#_baseline = psutil.virtual_memory().used >> 10
 
-
+	_recursiveCalls = 0
 	_peak = 0
 
 def update_peak():
@@ -21,6 +21,14 @@ def update_peak():
 	mem = _proc.memory_info().rss >> 10
 	_peak = max(_peak, (abs(mem-_baseline)))
 
+def update_calls(calls):
+	global _recursiveCalls
+	_recursiveCalls = calls
+
+
+def get_calls():
+	global _recursiveCalls
+	return _recursiveCalls
 def getPeak():
 	global _peak
 	return _peak
