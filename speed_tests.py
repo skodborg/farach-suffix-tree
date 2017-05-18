be435665626929ca8e85a596b7669d220d32d293
Simon Fischer <mail@simonfischer.com>
2017-05-18 16:43:24 +0200

    made tests ready for alphabet size

--- speed_tests.py
+++ speed_tests.py
@@ -43,34 +43,34 @@ def getAverageForString(S, algorithm):
 
 
 def testProcedure(algorithms, data_functions):
-	iterations = 5
+	iterations = 3
 	# maxTime = 5*60  #sekunder
 
 	timeTaken = dict()
 
-	# for i in [range(10*1000, 3400000000*1000, 40*1000)]:
-	for i in range(2, 50):
-		a_size = 2**i
+	for i in range(10*1000, 34000000000*1000, 10*1000):
 
 		for data_func in data_functions:
 			# data = data_func(i)
-			rnd_data = data_func(a_size)
+			# rnd_data = data_func(a_size)
 
 			for alg in algorithms:
 			
-				for t in range(iterations):
-					data = rnd_data(100*1000)
+				for i_pow in range(14, 18):
+					for _ in range(iterations):
+						data = data_func(2**i_pow)(i)
 
-					start = time.time()
+						start = time.time()
 
-					alg.construct_suffix_tree(data)
-					end = time.time()
-					print(len(data))
-					t = end-start
+						tree = alg.construct_suffix_tree(data)
+						end = time.time()
+						t = end-start
+						totalNodes = tree.totalNodes() - len(data)
 
-					f = open("testData/2" + alg.__name__ + "_" + data_func.__name__, 'a')
-					f.write(str(i)+", "  + str(t)  +  "\n") 
-					f.close()
+						f = open("testData/compare_" + str(i_pow) + "_" + alg.__name__ + "_" + data_func.__name__, 'a')
+						f.write(str(i)+", "  + str(t) + "," + str(totalNodes) +  "\n") 
+						f.close()
+		print(i)
 
 def test_varying_alphabet_size():
 	iterations = 5
@@ -159,7 +159,9 @@ def random_data_varying_alphabet(length):
 
 			return str2int([str(random.randint(0,length)) for _ in range(i)])
 		return helper
-
+def random_data_fixed_alphabet(i):
+	return str2int([str(random.randint(0,20*1000)) for _ in range(i)])
+	
 def same_char(length):
 	return [0]*length
 
@@ -169,15 +171,31 @@ def same_char(length):
 
 def main():
 	#testMemoryTracking()
-	# #testNoise()
-	# S = different_char(10*1000)
-	# start = time.time()
-	# farach.construct_suffix_tree(S)
-	# print(time.time() - start)
-	# join(random.choice([str(n) for n in range(i)]) for _ in range(i))
-	testProcedure([naive, farach, mccreight], [random_data_varying_alphabet])
-	# test_varying_alphabet_size()
-	# testProcedure([farach], [random_data])
+	#testNoise()
+	
+	# for i in range(10,24):
+
+	# 	data_func = random_data_varying_alphabet(2**i)
+	# 	S = data_func(10*1000)
+	# 	chars = set()
+	# 	for c in S:
+	# 		chars.add(c)
+
+	# 	start = time.time()
+	# 	tree = naive.construct_suffix_tree(S)
+	# 	print("-"*50)
+	# 	print("num of children on averge: " + str(tree.averageLeafs()))
+	# 	print("uniq len: " + str(len(chars)))
+	# 	print("leafs from root: " + str(tree.leafs_from_root()))
+	# 	totalN = tree.totalNodes()
+	# 	print("total nodes: " + str(totalN))
+	# 	print("inner node: " + str(totalN-len(S)))
+	# 	print("time: " + str(time.time()-start))
+	# 	print("depth: " + str(tree.depth()))
+		# join(random.choice([str(n) for n in range(i)]) for _ in range(i))
+	testProcedure([farach, mccreight], [random_data_varying_alphabet])
+		# test_varying_alphabet_size()
+		# testProcedure([farach], [random_data])
 
 
 if __name__ == '__main__':
